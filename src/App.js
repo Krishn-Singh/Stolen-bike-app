04866c315a980dc2c4e61c6cd61504f6159be929
krishn-Singh <krishnavirsingh29@gmail.com>
2022-10-12 02:06:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Card from './components/Card/Card'
+
+
+function App() {
+  return (
+    <div className="App">
+    <  Navbar />
+    <  Card />
+    </div>
+  );
+}
+
+export default App;
